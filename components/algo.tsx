26bb743db78b133c1e56d8cb823073a8c24b3804
risingbirdsong <heregoingthere@outlook.com>
2020-07-02 17:54:10 -0700

    ugly, but the modal is displaying

--- components/algo.tsx
+++ components/algo.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { LeftBrace, RightBrace, Space } from "./brace";
+import VisualizeCode from "./visualizeCode";
 
 interface AlgoI {
   algoString: string;
@@ -122,6 +123,62 @@ const Algo = (props: AlgoI): JSX.Element => {
           run algorithm
         </button>
       )}
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-toggle="modal"
+        data-target="#exampleModalCenter"
+      >
+        Launch demo modal
+      </button>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-toggle="modal"
+        data-target="#thecodeblock"
+      >
+        Launch Code
+      </button>
+      <div
+        className="modal fade"
+        id="exampleModalCenter"
+        tabIndex={-1}
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLongTitle">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">...</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <VisualizeCode />
     </div>
   );
 };
--- index.tsx
+++ index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import ReactDom from "react-dom";
 import App from "./app";
+import VisualizeCode from "./components/visualizeCode";
 
+// ReactDom.render(<VisualizeCode />, document.getElementById("root"));
 ReactDom.render(<App />, document.getElementById("root"));
