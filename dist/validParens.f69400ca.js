2b9fc9350ee6fd90258f5a3b98dac218f8187597
risingbirdsong <heregoingthere@outlook.com>
2020-07-21 07:54:51 -0700

    added favicon and document title

--- dist/validParens.f69400ca.js
+++ dist/validParens.f69400ca.js
@@ -75223,7 +75223,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64734" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50627" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
